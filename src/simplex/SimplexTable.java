8ff471eed42c6e88192e8fa30d9ecdbb33415c80
Massa <massa_@msn.com>
2016-04-21 02:09:10 -0300

    add simplex table mounting logic

--- src/simplex/SimplexTable.java
+++ src/simplex/SimplexTable.java
@@ -2,10 +2,69 @@ package simplex;
 
 class SimplexTable {
 
+    Expression e;
     private Cell[][] simplexTable;
+    private int[] basicVariables;
+    private int[] nonBasicVariables;
 
     public SimplexTable(Expression e) {
+        this.e = e;
+        this.createSimplexTable();
+        this.populateObjectiveFunction();
+        this.populateConstraints();
+        this.setBasicVariables();
+        this.setNonBasicVariables();
+    }
 
+    private void createSimplexTable() {
+        this.basicVariables = new int[e.countBasicVariables()];
+        this.nonBasicVariables = new int[e.countNonBasicVariables()];
+        int rows = basicVariables.length + 1;//+ objective function
+        int cols = nonBasicVariables.length + 1;//+ 1 col for looseness/excess
+        simplexTable = new Cell[rows][cols];
+    }
+
+    private void populateObjectiveFunction() {
+        simplexTable[0][0] = new Cell();
+
+        int[] objectiveFunction = e.getObjectiveFunction();
+        int multiplier;
+        if (e.getObjective() == Expression.OBJECTIVE_MAX) {
+            multiplier = 1;
+        } else {
+            multiplier = -1;
+        }
+
+        for ( int i = 1; i <= objectiveFunction.length; i+=1 ) {
+            simplexTable[0][i] = new Cell(objectiveFunction[i-1]*multiplier);
+        }
+    }
+    
+    private void populateConstraints() {
+        for ( int i = 1; i <= basicVariables.length; i+=1) {
+            int multiplier;
+            if ( e.getConstraintSign(i-1) == Expression.CONSTRAINT_SIGN_GT ) {
+                multiplier = -1;
+            } else {
+                multiplier = 1;
+            }
+            simplexTable[i][0] = new Cell(e.getB(i-1)*multiplier);
+            for ( int j = 1; j <= nonBasicVariables.length; j+=1) {
+                simplexTable[i][j] = new Cell(e.getConstraint(i-1,j-1)*multiplier);
+            }
+        }
+    }
+    
+    private void setBasicVariables() {
+    	for (int i = 0; i < basicVariables.length; i+=1 ) {
+    		basicVariables[i] = i;
+    	}
+    }
+    
+    private void setNonBasicVariables() {
+    	for (int i = 0; i < nonBasicVariables.length; i+=1 ) {
+    		nonBasicVariables[i] = i + basicVariables.length;
+    	}
     }
 
     public int getNegativeFreeMemberIndex() {
@@ -82,6 +141,11 @@ class SimplexTable {
         }
 
         return permittedLine;
+    }
+
+
+    public Cell[][] getSimplexTable() {
+        return simplexTable;
     }
 
 }
